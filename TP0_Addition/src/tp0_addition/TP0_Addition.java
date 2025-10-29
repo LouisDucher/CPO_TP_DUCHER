2cfa88e7ed311ed5f0b305ac51de174f3575f70f
Louis Ducher <louis.ducher@epfedu.fr>
2025-10-29 22:06:28 +0100

    Création d'un nouveau projet versionné

---
+++ TP0_Addition/src/tp0_addition/TP0_Addition.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_addition;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author louis
+ */
+public class TP0_Addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nb; // nombre d'entiers à additionner
+        Scanner sc = new Scanner(System.in);
+System.out.println("Entrer le nombre :");
+nb=sc.nextInt();
+    
+int result; // resultat
+int ind; //indice
+// initialisation
+result=0;
+// addition des nb premiers entiers
+ind=1;
+while (ind< nb) {
+ result=result+ind;
+ ind++;
+}
+// affichage du resultat
+System.out.println("la somme des "+ nb + "entiers est:"+result); }
+    
+    }
+
