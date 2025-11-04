215586397f6fd6351f6c40e80c9e286c97690dd3
Louis Ducher <louis.ducher@epfedu.fr>
2025-11-04 12:15:45 +0100

    Création d'un nouveau projet versionné

--- TP2_Bieres_DUCHER/src/tp2_bieres_ducher/BouteilleBiere.java
+++ TP2_Bieres_DUCHER/src/tp2_bieres_ducher/BouteilleBiere.java
@@ -23,13 +23,22 @@ public class BouteilleBiere {
     brasserie = uneBrasserie;
     ouverte = false; } 
     void Décapsuler(){
-        if (ouverte != false){ 
+        if (ouverte == false){ 
                 ouverte = true;}
                 else{
               System.out.println(" erreur : biere déjà ouverte");
               System.out.print(false)   ;
+              
                        }
-        }
+        
+        } @Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = nom + " (" + degreAlcool + " degré Ouverte ?) ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+}
     }
 
 
