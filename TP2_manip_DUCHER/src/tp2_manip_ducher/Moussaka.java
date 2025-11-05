2256e66554d7eb4bb410a525836de7e5059ed9a2
Louis Ducher <louis.ducher@epfedu.fr>
2025-11-05 12:33:56 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_manip_DUCHER/src/tp2_manip_ducher/Moussaka.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_ducher;
+
+/**
+ *
+ * @author louis
+ */
+public class Moussaka {
+    int nbCalories;
+
+    public Moussaka(int nbCalories) {
+        this.nbCalories = nbCalories;
+    }
+    
+    
+}
